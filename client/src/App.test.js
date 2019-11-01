53b39d705dd393e663ff7e83112d43373f023db6
Sam <Jeffcoat>
2019-11-01 12:46:05 -0500

    Attempts some tests, failed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-it('renders without crashing', () => {
+afterEach(rtl.cleanup);
+
+
+it(`Renders without crashing, has class name of 'App'`, () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  rtl.render(<App />, div)
+  rtl.unmountComponentAtNode(div);
 });
+
+it('loads dark mode button', () => {
+  const app = rtl.render(<App/>);
+  const element = app.getByTitle('dark-mode-btn');
+  expect(element).toBeVisible();
+})
